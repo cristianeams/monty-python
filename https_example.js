005064a7d85b9022ebc5cfe48ce956f36c5214d6
Cristiane Silva <cristianeavelarmaia@gmail.com>
2018-06-10 23:55:23 +0000

    response example

--- https_example.js
+++ https_example.js
@@ -1,5 +1,5 @@
 var https = require('https');
-console.log('I did it!');
+//console.log('I did it!');
 
 var options = {
     host: 'www.example.org',
@@ -7,8 +7,10 @@ var options = {
 };
 
 //called by https when the request is made
-var callback = function() {
+//callback needs a responde argument to read the response
+var callback = function(response) {
     console.log('In responde handler callback!');
+    console.log('Response: ', response);
 }
 
 console.log("I'm about to make the request!");
