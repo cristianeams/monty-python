ce516527ed33e90a0ceef86a252aa669b75fe4fa
Cristiane Silva <cristianeavelarmaia@gmail.com>
2018-06-11 00:00:55 +0000

    prints chunks of data

--- https_example.js
+++ https_example.js
@@ -8,11 +8,21 @@ var options = {
 
 //called by https when the request is made
 //callback needs a responde argument to read the response
+// var callback = function(response) {
+//     console.log('In responde handler callback!');
+//     console.log('Response: ', response);
+// }
+
 var callback = function(response) {
     console.log('In responde handler callback!');
-    console.log('Response: ', response);
+    //outputs the data as it comes in
+    //label response data as chunk
+    //prints the length of string
+    response.on('data', function(chunk) {
+        console.log('[-- CHUNK OF LENGTH ' + chunk.length + '--]');
+        console.log(chunk.toString());
+    });
 }
-
 console.log("I'm about to make the request!");
 
 https.request(options,callback).end();
